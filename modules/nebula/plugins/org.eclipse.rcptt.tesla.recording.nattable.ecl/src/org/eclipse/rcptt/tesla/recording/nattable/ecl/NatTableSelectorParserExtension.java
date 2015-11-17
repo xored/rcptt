a23bfd658e9c8ffc024374ca5c123608916c5bc9
Andreas Fackler <andreas.fackler@xored.com>
2015-11-17 19:05:57 +0200

    Add NatTable plug-ins to the nebula features.
    Change-Id: I811ff44506b5cf0ff1111da7affec8e527f2b2be

--- modules/nebula/plugins/org.eclipse.rcptt.tesla.recording.nattable.ecl/src/org/eclipse/rcptt/tesla/recording/nattable/ecl/NatTableSelectorParserExtension.java
+++ modules/nebula/plugins/org.eclipse.rcptt.tesla.recording.nattable.ecl/src/org/eclipse/rcptt/tesla/recording/nattable/ecl/NatTableSelectorParserExtension.java
@@ -12,14 +12,9 @@ package org.eclipse.rcptt.tesla.recording.nattable.ecl;
 
 import java.util.List;
 
-import org.eclipse.rcptt.tesla.ecl.model.GetCell;
-import org.eclipse.rcptt.tesla.ecl.model.GetColumnHeader;
 import org.eclipse.rcptt.tesla.ecl.model.Selector;
-import org.eclipse.rcptt.tesla.ecl.model.TeslaFactory;
 import org.eclipse.rcptt.tesla.nattable.ecl.NebulaNatTableElementKinds;
-import org.eclipse.rcptt.tesla.nattable.ecl.nattable.GetRowHeader;
 import org.eclipse.rcptt.tesla.nattable.ecl.nattable.NattableFactory;
-import org.eclipse.rcptt.tesla.nattable.model.NatTableCellPosition;
 import org.eclipse.rcptt.tesla.recording.core.ecl.ISelectorParserExtension;
 
 /**
@@ -31,22 +26,6 @@ public class NatTableSelectorParserExtension implements ISelectorParserExtension
 	public Selector selector(String customKindId, String text, List<String> path, List<Integer> indexes) {
 		if (NebulaNatTableElementKinds.NAT_TABLE.equals(customKindId)) {
 			return NattableFactory.eINSTANCE.createGetNatTable();
-		} else if (NebulaNatTableElementKinds.NAT_TABLE_CELL.equals(customKindId)) {
-			NatTableCellPosition position = NatTableCellPosition.fromPath(path.get(0));
-			if (position.getCol() == -1) {
-				GetColumnHeader selector = TeslaFactory.eINSTANCE.createGetColumnHeader();
-				selector.setText(position.getText());
-				return selector;
-			} else if (position.getRow() == -1) {
-				GetRowHeader selector = NattableFactory.eINSTANCE.createGetRowHeader();
-				selector.setText(position.getText());
-				return selector;
-			} else {
-				GetCell getCell = TeslaFactory.eINSTANCE.createGetCell();
-				getCell.setRow(position.getRow());
-				getCell.setColumn(position.getCol());
-				return getCell;
-			}
 		}
 		return null;
 	}
