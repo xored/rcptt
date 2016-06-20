27cc3a7b63be43eaf3fc0e323f1c807cee1fc177
Kovalev Artem <nomit007@gmail.com>
2016-06-20 14:35:27 +0600

    rap-3.1 Add Rap Tesla command bundle

---
+++ core/org.eclipse.rcptt.tesla.ecl.rap/gen-src/org/eclipse/rcptt/tesla/ecl/rap/model/RapTeslaFactory.java
@@ -0,0 +1,69 @@
+/**
+ */
+package org.eclipse.rcptt.tesla.ecl.rap.model;
+
+import org.eclipse.emf.ecore.EFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Factory</b> for the model.
+ * It provides a create method for each non-abstract class of the model.
+ * <!-- end-user-doc -->
+ * @see org.eclipse.rcptt.tesla.ecl.rap.model.RapTeslaPackage
+ * @generated
+ */
+public interface RapTeslaFactory extends EFactory {
+	/**
+	 * The singleton instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	RapTeslaFactory eINSTANCE = org.eclipse.rcptt.tesla.ecl.rap.model.impl.RapTeslaFactoryImpl.init();
+
+	/**
+	 * Returns a new object of class '<em>Run Js</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Run Js</em>'.
+	 * @generated
+	 */
+	RunJs createRunJs();
+
+	/**
+	 * Returns a new object of class '<em>Exec Without Js</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Exec Without Js</em>'.
+	 * @generated
+	 */
+	ExecWithoutJs createExecWithoutJs();
+
+	/**
+	 * Returns a new object of class '<em>Set Download Result File</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Set Download Result File</em>'.
+	 * @generated
+	 */
+	SetDownloadResultFile createSetDownloadResultFile();
+
+	/**
+	 * Returns a new object of class '<em>Verify Download File</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Verify Download File</em>'.
+	 * @generated
+	 */
+	VerifyDownloadFile createVerifyDownloadFile();
+
+	/**
+	 * Returns the package supported by this factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the package supported by this factory.
+	 * @generated
+	 */
+	RapTeslaPackage getRapTeslaPackage();
+
+} //RapTeslaFactory
---
+++ core/org.eclipse.rcptt.tesla.ecl.rap/gen-src/org/eclipse/rcptt/tesla/ecl/rap/model/impl/VerifyDownloadFileImpl.java
@@ -0,0 +1,165 @@
+/**
+ */
+package org.eclipse.rcptt.tesla.ecl.rap.model.impl;
+
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+
+import org.eclipse.rcptt.ecl.core.impl.CommandImpl;
+
+import org.eclipse.rcptt.tesla.ecl.rap.model.RapTeslaPackage;
+import org.eclipse.rcptt.tesla.ecl.rap.model.TeslaPackage;
+import org.eclipse.rcptt.tesla.ecl.rap.model.VerifyDownloadFile;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Verify Download File</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * </p>
+ * <ul>
+ *   <li>{@link org.eclipse.rcptt.tesla.ecl.rap.model.impl.VerifyDownloadFileImpl#getUrl <em>Url</em>}</li>
+ * </ul>
+ *
+ * @generated
+ */
+public class VerifyDownloadFileImpl extends CommandImpl implements VerifyDownloadFile {
+	/**
+	 * The default value of the '{@link #getUrl() <em>Url</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getUrl()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String URL_EDEFAULT = null;
+
+	/**
+	 * The cached value of the '{@link #getUrl() <em>Url</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getUrl()
+	 * @generated
+	 * @ordered
+	 */
+	protected String url = URL_EDEFAULT;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected VerifyDownloadFileImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return RapTeslaPackage.Literals.VERIFY_DOWNLOAD_FILE;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getUrl() {
+		return url;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setUrl(String newUrl) {
+		String oldUrl = url;
+		url = newUrl;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, RapTeslaPackage.VERIFY_DOWNLOAD_FILE__URL, oldUrl, url));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case RapTeslaPackage.VERIFY_DOWNLOAD_FILE__URL:
+				return getUrl();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case RapTeslaPackage.VERIFY_DOWNLOAD_FILE__URL:
+				setUrl((String)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case RapTeslaPackage.VERIFY_DOWNLOAD_FILE__URL:
+				setUrl(URL_EDEFAULT);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case RapTeslaPackage.VERIFY_DOWNLOAD_FILE__URL:
+				return URL_EDEFAULT == null ? url != null : !URL_EDEFAULT.equals(url);
+		}
+		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		if (eIsProxy()) return super.toString();
+
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (url: ");
+		result.append(url);
+		result.append(')');
+		return result.toString();
+	}
+
+} //VerifyDownloadFileImpl
