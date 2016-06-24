cec71d495255e1cdfa65debe10b206b7f3f97e24
Kovalev Artem <nomit007@gmail.com>
2016-06-24 11:06:13 +0600

    rap-3.1: Fix dependencise build

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -3,4 +3,4 @@ export MAVEN_OPTS="-Xms512m -Xmx756m -XX:MaxPermSize=256m"
 
 OPTIONS="-Dtycho.localArtifacts=ignore $@"
 
-mvn clean verify -f releng/mirroring/pom.xml $OPTIONS || exit 100
+mvn clean verify -f releng/mirroring/pom.xml $OPTIONS || exit 100 
