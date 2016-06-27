6e84730bcd6563b8dc5fd80c86567bda5dd80ff9
Kovalev Artem <nomit007@gmail.com>
2016-06-27 11:39:09 +0600

    rap-3.1 Add rap module build script

--- modules/rap/bundles/core/org.eclipse.rcptt.tesla.recording.core.ecl.rap/src/org/eclipse/rcptt/tesla/recording/core/ecl/rap/parser/RapTeslaParser.java
+++ modules/rap/bundles/core/org.eclipse.rcptt.tesla.recording.core.ecl.rap/src/org/eclipse/rcptt/tesla/recording/core/ecl/rap/parser/RapTeslaParser.java
@@ -3,6 +3,7 @@ package org.eclipse.rcptt.tesla.recording.core.ecl.rap.parser;
 import org.eclipse.rcptt.ecl.core.Command;
 import org.eclipse.rcptt.ecl.core.Sequence;
 import org.eclipse.rcptt.ecl.core.util.ScriptletFactory;
+import org.eclipse.rcptt.tesla.core.protocol.RapDownloadFile;
 import org.eclipse.rcptt.tesla.ecl.rap.model.ExecWithoutJs;
 import org.eclipse.rcptt.tesla.ecl.rap.model.RapTeslaFactory;
 import org.eclipse.rcptt.tesla.ecl.rap.model.SetDownloadResultFile;
@@ -12,7 +13,7 @@ import org.eclipse.rcptt.tesla.recording.core.ecl.TeslaCommand;
 public class RapTeslaParser {
 
 	@TeslaCommand(packageUri="", classifier ="")
-	public Command c(RapDownloadFile file)
+	public Command downloadFile(RapDownloadFile file)
 	{
 			final ExecWithoutJs command = RapTeslaFactory.eINSTANCE.createExecWithoutJs();
 
