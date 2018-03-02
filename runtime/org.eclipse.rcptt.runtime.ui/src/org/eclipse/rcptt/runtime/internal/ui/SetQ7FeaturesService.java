e76831e12b461a75ec791a7ac57625a340ce04d3
Viktoria Dlugopolskaya <viksnsk@gmail.com>
2018-03-02 15:54:34 +0700

    Moved SetQ7FeaturesService to e3 runtime, created a new service for e4

--- runtime/org.eclipse.rcptt.runtime.ui/src/org/eclipse/rcptt/runtime/internal/ui/SetQ7FeaturesService.java
+++ runtime/org.eclipse.rcptt.runtime.ui/src/org/eclipse/rcptt/runtime/internal/ui/SetQ7FeaturesService.java
@@ -8,21 +8,18 @@
  * Contributors:
  *     Xored Software Inc - initial API and implementation and/or initial documentation
  *******************************************************************************/
-package org.eclipse.rcptt.internal.runtime;
+package org.eclipse.rcptt.runtime.internal.ui;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.jface.bindings.keys.formatting.KeyFormatterFactory;
-import org.eclipse.rcptt.core.Q7Features;
 import org.eclipse.rcptt.core.ecl.core.model.SetQ7Features;
 import org.eclipse.rcptt.ecl.core.Command;
 import org.eclipse.rcptt.ecl.runtime.ICommandService;
 import org.eclipse.rcptt.ecl.runtime.IProcess;
-import org.eclipse.rcptt.tesla.core.TeslaFeatures;
-import org.eclipse.rcptt.tesla.core.TeslaVariables;
-import org.eclipse.rcptt.tesla.swt.events.TeslaEventManager;
+import org.eclipse.rcptt.runtime.RcpttFeatureUtils;
 import org.eclipse.rcptt.tesla.workbench.formatter.Q7KeyFormatter;
 
 public class SetQ7FeaturesService implements ICommandService {
@@ -31,34 +28,16 @@ public class SetQ7FeaturesService implements ICommandService {
 			throws InterruptedException, CoreException {
 		SetQ7Features service = (SetQ7Features) command;
 		EList<String> features = service.getFeatures();
+		RcpttFeatureUtils.setFeatures(features);
 
-		checkStatusDialogFeature(features);
-
-		Q7Features.getInstance().setValues(features);
-		TeslaFeatures.getInstance().setValues(features);
-
-		TeslaVariables.updateFromFeatureList(features);
-
-		// TODO (e4 support): move to a better place?
 		checkShortcutsFormatter();
 
 		return Status.OK_STATUS;
-	}
-
-	private void checkStatusDialogFeature(EList<String> features) {
-		for (String feature : features) {
-			int pos = feature.indexOf('=');
-			String key = feature.substring(0, pos);
-			String value = feature.substring(pos + 1);
-			if (key.equals(TeslaFeatures.STATUS_DIALOG_ALLOWED)) {
-				TeslaEventManager.getManager().setStatusDialogModeAllowed(
-						Boolean.valueOf(value));
-			}
-		}
 	}
 
 	private void checkShortcutsFormatter() {
 		if (!(KeyFormatterFactory.getDefault() instanceof Q7KeyFormatter))
 			Q7KeyFormatter.installQ7Formatter();
 	}
+
 }
